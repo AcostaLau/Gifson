71e24454e66e559fcba4255cf5ee2f515cc1baab
Lautaro Acosta <soylautaroacosta@gmail.com>
2022-06-25 18:51:05 -0300

    deploy

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import styled from 'styled-components'
 import '../style.css'
 import { Link } from 'react-router-dom'
